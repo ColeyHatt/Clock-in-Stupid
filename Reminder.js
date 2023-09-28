a913184093a2139a45d8ba3c997ec4075087a3c4
Coley Hatt <122749638+ColeyHatt@users.noreply.github.com>
2023-09-28 13:34:32 -0400

    Update and rename Reminder1.js to Reminder.js

---
+++ Reminder.js
@@ -0,0 +1,10 @@
+function rem() {
+  MailApp.sendEmail("coley@digitalsky.us.com",
+                  "Clock in",
+                  "Please Clock in if you haven't!");
+  }
+function rem2() {
+  MailApp.sendEmail("coley@digitalsky.us.com",
+                  "Clock out",
+                  "Please Clock out if you haven't!");
+  }
--- Reminder1.js
+++
@@ -1,5 +0,0 @@
-function rem() {
-  MailApp.sendEmail("coley@digitalsky.us.com",
-                  "Clock in",
-                  "Please Clock in if you haven't!");
-  }
